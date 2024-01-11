5939d130e7e459a6e4ea83652779dfed6301cbcd
gavrilariendra <gavrilariendra00@gmail.com>
2024-01-11 21:31:34 +0700

    fix: fixing layout for sidebar

--- spotify-clone/components/Sidebar.tsx
+++ spotify-clone/components/Sidebar.tsx
@@ -46,7 +46,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           <Library />
         </Box>
       </div>
-      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+      <main className="h-full flex-1 overflow-y-auto py-2 pr-2">{children}</main>
     </div>
   );
 };
