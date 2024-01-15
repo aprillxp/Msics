0f92ae0f94dc1cb6eb99b0cfae7e4a0848dcee71
gavrilariendra <gavrilariendra00@gmail.com>
2024-01-15 22:34:57 +0700

    add mediaItem

--- spotify-clone/components/Library.tsx
+++ spotify-clone/components/Library.tsx
@@ -5,8 +5,14 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -18,7 +24,7 @@ const Library = () => {
 
     // Check for subscription
 
-    return uploadModal.onOpen()
+    return uploadModal.onOpen();
   };
 
   return (
@@ -34,7 +40,16 @@ const Library = () => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">List of songs!</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((item) => (
+          
+          <MediaItem
+            onClick={() => {}}
+            key={item.id}
+            data={item}
+          />
+        ))}
+      </div>
     </div>
   );
 };
